81dbfd50d9685889d1a9518d18e0a698d62cde31
Mitohku <34718938+Mitohku@users.noreply.github.com>
2018-03-26 20:37:02 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,8 +15,8 @@ import aiohttp
 from collections                import Counter
 
 
-command_prefix = "!" #CHANGE IT TO WHAT YOU WANT
-description = "Bot Privé de la 4ème Dimension" #ALSO CHANGE THIS
+command_prefix = "!" 
+description = "Bot Privé de la 4ème Dimension"
 bot = commands.Bot(command_prefix, description = description)
 bot.remove_command('help')
 tu = datetime.datetime.now()
@@ -36,6 +36,19 @@ async def test(ctx):
 	embed.description = f"CECI EST UNE COMMANDE DE TEST"
 	embed.set_footer(text = "TEST")
 	await ctx.send(embed=embed)
+
+@bot.command(aliases = ['team'])
+async def staff(ctx):
+	embed1 = discord.Embed(colour = discord.Colour(0xC21C1C))
+	embed2 = discord.Embed(colour = discord.Colour(0xC21C1C))
+	embed3 = discord.Embed(colour = discord.Colour(0xC21C1C))
+	texte1 = "***/!\ ROLES & DESCRIPTION DU STAFF /!\***"
+	texte2 = "***/!\ ROLES & DESCRIPTION DU NOYAU DUR /!\***"
+
+	await ctx.send(texte1)
+	embed1.set_author(name = "Foreigners 4D")
+	embed1.description = f"Say"
+	embed1.add_field(name="Description", value= f"- **__Rôle Principal:__** Ils sont intégrés au sein du staff en statut de formation, sous une attente d'une semaine maximum. Jeni (Responsable des animateurs/helpers) valide ou non l'intégration définitive.\n- **__Les attentes:__** s'occuper des **Sans Papiers** (accueillir les nouveaux), dynamiser le serveur, faire des invites de masse.\n- **__Droits :__**  mutes/unmute sur les salons vocaux, ajouts de rôles.", inline=False)
 
 
 #----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
