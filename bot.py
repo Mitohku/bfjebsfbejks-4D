085233e7070bdb6062c5d284e8d8a72edb2285da
Mitohku <34718938+Mitohku@users.noreply.github.com>
2018-03-27 14:00:49 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -39,66 +39,77 @@ async def test(ctx):
 
 @bot.command(aliases = ['team'])
 async def staff(ctx):
-	embed1 = discord.Embed(colour = discord.Colour(0xC21C1C))
-	embed2 = discord.Embed(colour = discord.Colour(0xC21C1C))
-	embed3 = discord.Embed(colour = discord.Colour(0xC21C1C))
-	embed4 = discord.Embed(colour = discord.Colour(0xC21C1C))
-	embed5 = discord.Embed(colour = discord.Colour(0xC21C1C))
-	embed6 = discord.Embed(colour = discord.Colour(0xC21C1C))
-	embed7 = discord.Embed(colour = discord.Colour(0xC21C1C))
-	embed8 = discord.Embed(colour = discord.Colour(0xC21C1C))
-	embed9 = discord.Embed(colour = discord.Colour(0xC21C1C))
-	texte1 = "***/!\ ROLES & DESCRIPTION DU STAFF /!\***"
-	texte2 = "***/!\ ROLES & DESCRIPTION DU NOYAU DUR /!\***"
+
+	if await ctx.bot.is_owner(ctx.author):
+		developer = bot.get_user(385419569558323202) # commands.get_user(commands.owner_id)
+		shutdown1 = discord.Embed(colour = discord.Colour(0xA522B3))
+		shutdown1.set_author(name='[SHUTDOWN]', url = "https://prestig-web.wixsite.com/spirit-bot")
+		shutdown1.description = f"**Spirit | 精霊** has been **Shutdown For Maintenance** by ***{developer.name}*** ! <:Maintenance:391999920229318668>"
+		shutdown1.set_footer(text = time.strftime("%d/%m/%Y - %I:%M:%S %p"))
+		
+		await ctx.send(embed = shutdown1)
+		
+    else:
+		embed1 = discord.Embed(colour = discord.Colour(0xC21C1C))
+		embed2 = discord.Embed(colour = discord.Colour(0xC21C1C))
+		embed3 = discord.Embed(colour = discord.Colour(0xC21C1C))
+		embed4 = discord.Embed(colour = discord.Colour(0xC21C1C))
+		embed5 = discord.Embed(colour = discord.Colour(0xC21C1C))
+		embed6 = discord.Embed(colour = discord.Colour(0xC21C1C))
+		embed7 = discord.Embed(colour = discord.Colour(0xC21C1C))
+		embed8 = discord.Embed(colour = discord.Colour(0xC21C1C))
+		embed9 = discord.Embed(colour = discord.Colour(0xC21C1C))
+		texte1 = "***/!\ ROLES & DESCRIPTION DU STAFF /!\***"
+		texte2 = "***/!\ ROLES & DESCRIPTION DU NOYAU DUR /!\***"
 	
 
-	await ctx.send(texte1)
-	embed1.set_author(name = "Foreigners 4D")
-	embed1.description = f"Say"
-	embed1.add_field(name="Description", value= f"- **__Rôle Principal:__** Ils sont intégrés au sein du staff en statut de formation, avec une attente d'une semaine maximum. Jeni *(Responsable des **Animateurs**/**Helpers***)* valide ou non l'intégration définitive.\n- **__Les attentes:__** s'occuper des **Sans Papiers** (accueillir les nouveaux), dynamiser le serveur, faire des invitations de masse.\n- **__Droits:__** mute/unmute sur les salons vocaux, ajouts de rôles.", inline=False)
-	embed1.set_footer(text = "| ©  Say' (1/5)")
-	embed2.set_author(name = "Animators / Helpers")
-	embed2.description = f"Alone **-** Lurewing **-** Reyss **-** K "
-	embed2.add_field(name="Description", value= f"- **__Rôle Principal:__** Ce sont des **Animateurs**/**Helpers** confirmés\n- **__Les attentes:__** Ils doivent s'occuper des **Sans Papiers** (accueillir les nouveaux), dynamiser le serveur, faire des invitations de masse.\n- **__Droits:__** mute/unmute sur les salons vocaux, ajouts de rôles, expulser un membre.", inline=False)
-	embed2.set_footer(text = "| ©  Say' (2/5)")
-	embed3.set_author(name = "Moderators")
-	embed3.description = f"Savy **-** Ascian"
-	embed3.add_field(name="Description", value= f"- **__Rôle Principal:__** Ils encadrent les **Animateurs** et les **Foreigners** de manière technique et relationnelle.\n- **__Les attentes:__** Ils doivent dynamiser le serveur, faire des invitations de masse. Ils jouent le rôle de médiateur entre membres, **Foreigners** et **Animateurs**\n- **__Droits:__** mute/unmute sur les salons vocaux, ajouts de rôles *(**+18 NSFW**)*, expulser un membre.", inline=False)
-	embed3.set_footer(text = "| ©  Say' (3/5)")
-	embed4.set_author(name = "Operators")
-	embed4.description = f"Dom **-** Jeni "
-	embed4.add_field(name="Description", value= f"- **__Rôle Principal:__** Dom gère la zone +18 *(trash, section adulte, etc..)*\n- **__Les attentes:__** Ils sont autant proches des nouveaux membres que du Fondateur. La prise du recul et la priorisation sont leurs armes.\n- **__Droits:__** mute/unmute sur les salons vocaux, ajouts de rôles *(**+18 NSFW**)*, expulser un membre.", inline=False)
-	embed4.set_footer(text = "| ©  Say' (4/5)")
-	embed5.set_author(name = "Administrators")
-	embed5.description = f"Cancer **-** NONOXX **-** Skippy **-** Ashley"
-	embed5.add_field(name="Description", value= f"- **__Rôle Principal:__** ???\n- **__Les attentes:__** Ils encadrent les **Modérateurs**, les **Animateurs**/**Helpers** et les **Foreigners** de manière technique et relationnelle.\n- **__Droits:__** mute/unmute sur les salons vocaux, ajouts de rôles *(**+18 NSFW**)*, expulser/bannir un membre.", inline=False)
-	embed5.set_footer(text = "| ©  Say' 1-(5/5)")
+		await ctx.send(texte1)
+		embed1.set_author(name = "Foreigners 4D")
+		embed1.description = f"Say"
+		embed1.add_field(name="Description", value= f"- **__Rôle Principal:__** Ils sont intégrés au sein du staff en statut de formation, avec une attente d'une semaine maximum. Jeni *(Responsable des **Animateurs**/**Helpers***)* valide ou non l'intégration définitive.\n- **__Les attentes:__** s'occuper des **Sans Papiers** (accueillir les nouveaux), dynamiser le serveur, faire des invitations de masse.\n- **__Droits:__** mute/unmute sur les salons vocaux, ajouts de rôles.", inline=False)
+		embed1.set_footer(text = "| ©  Say' (1/5)")
+		embed2.set_author(name = "Animators / Helpers")
+		embed2.description = f"Alone **-** Lurewing **-** Reyss **-** K "
+		embed2.add_field(name="Description", value= f"- **__Rôle Principal:__** Ce sont des **Animateurs**/**Helpers** confirmés\n- **__Les attentes:__** Ils doivent s'occuper des **Sans Papiers** (accueillir les nouveaux), dynamiser le serveur, faire des invitations de masse.\n- **__Droits:__** mute/unmute sur les salons vocaux, ajouts de rôles, expulser un membre.", inline=False)
+		embed2.set_footer(text = "| ©  Say' (2/5)")
+		embed3.set_author(name = "Moderators")
+		embed3.description = f"Savy **-** Ascian"
+		embed3.add_field(name="Description", value= f"- **__Rôle Principal:__** Ils encadrent les **Animateurs** et les **Foreigners** de manière technique et relationnelle.\n- **__Les attentes:__** Ils doivent dynamiser le serveur, faire des invitations de masse. Ils jouent le rôle de médiateur entre membres, **Foreigners** et **Animateurs**\n- **__Droits:__** mute/unmute sur les salons vocaux, ajouts de rôles *(**+18 NSFW**)*, expulser un membre.", inline=False)
+		embed3.set_footer(text = "| ©  Say' (3/5)")
+		embed4.set_author(name = "Operators")
+		embed4.description = f"Dom **-** Jeni "
+		embed4.add_field(name="Description", value= f"- **__Rôle Principal:__** Dom gère la zone +18 *(trash, section adulte, etc..)*\n- **__Les attentes:__** Ils sont autant proches des nouveaux membres que du Fondateur. La prise du recul et la priorisation sont leurs armes.\n- **__Droits:__** mute/unmute sur les salons vocaux, ajouts de rôles *(**+18 NSFW**)*, expulser un membre.", inline=False)
+		embed4.set_footer(text = "| ©  Say' (4/5)")
+		embed5.set_author(name = "Administrators")
+		embed5.description = f"Cancer **-** NONOXX **-** Skippy **-** Ashley"
+		embed5.add_field(name="Description", value= f"- **__Rôle Principal:__** ???\n- **__Les attentes:__** Ils encadrent les **Modérateurs**, les **Animateurs**/**Helpers** et les **Foreigners** de manière technique et relationnelle.\n- **__Droits:__** mute/unmute sur les salons vocaux, ajouts de rôles *(**+18 NSFW**)*, expulser/bannir un membre.", inline=False)
+		embed5.set_footer(text = "| ©  Say' 1-(5/5)")
 	
-	await ctx.send(embed = embed1)
-	await ctx.send(embed = embed2)
-	await ctx.send(embed = embed3)
-	await ctx.send(embed = embed4)
-	await ctx.send(embed = embed5)
+		await ctx.send(embed = embed1)
+		await ctx.send(embed = embed2)
+		await ctx.send(embed = embed3)
+		await ctx.send(embed = embed4)
+		await ctx.send(embed = embed5)
 	
 	
-	await ctx.send(texte2)
-	embed6.set_author(name = "Cancer")
-	embed6.add_field(name="???", value= f"Gère la zone vocal du serveur", inline=False)
-	embed6.set_footer(text = "| ©  Say' 2-(1/4)")
-	embed7.set_author(name = "Skippy")
-	embed7.add_field(name="???", value= f"Valide  les recrutements (candidatures / évolutions rôles)  du staff.", inline=False)
-	embed7.set_footer(text = "| ©  Say' 2-(2/4)")
-	embed8.set_author(name = "NONOXX")
-	embed8.add_field(name="???", value= f"Gère tout ce qui est interface graphique du serveur (logos/emojis/etc..)", inline=False)
-	embed8.set_footer(text = "| ©  Say' 2-(3/4)")
-	embed9.set_author(name = "Ashley")
-	embed9.add_field(name="???", value= f"Gère la planification des évènements, les réseaux sociaux, le règlement. (back-up Fondatrice).", inline=False)
-	embed9.set_footer(text = "| ©  Say' 2-(4/4)")
+		await ctx.send(texte2)
+		embed6.set_author(name = "Cancer")
+		embed6.add_field(name="???", value= f"Gère la zone vocal du serveur", inline=False)
+		embed6.set_footer(text = "| ©  Say' 2-(1/4)")
+		embed7.set_author(name = "Skippy")
+		embed7.add_field(name="???", value= f"Valide  les recrutements (candidatures / évolutions rôles)  du staff.", inline=False)
+		embed7.set_footer(text = "| ©  Say' 2-(2/4)")
+		embed8.set_author(name = "NONOXX")
+		embed8.add_field(name="???", value= f"Gère tout ce qui est interface graphique du serveur (logos/emojis/etc..)", inline=False)
+		embed8.set_footer(text = "| ©  Say' 2-(3/4)")
+		embed9.set_author(name = "Ashley")
+		embed9.add_field(name="???", value= f"Gère la planification des évènements, les réseaux sociaux, le règlement. (back-up Fondatrice).", inline=False)
+		embed9.set_footer(text = "| ©  Say' 2-(4/4)")
 	
-	await ctx.send(embed = embed6)
-	await ctx.send(embed = embed7)
-	await ctx.send(embed = embed8)
-	await ctx.send(embed = embed9)
+		await ctx.send(embed = embed6)
+		await ctx.send(embed = embed7)
+		await ctx.send(embed = embed8)
+		await ctx.send(embed = embed9)
 
 
 #----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
