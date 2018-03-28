9dfd977419584f7848fe67c1f2647d969a52149a
Mitohku <34718938+Mitohku@users.noreply.github.com>
2018-03-28 17:54:46 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -122,8 +122,8 @@ async def radio(ctx):
 
 @bot.command(aliases = ['anim', 'animation', 'evenement'])
 async def event(ctx):
-	karaoke = guild.channel.name(315219359368740875)
-	imitation = guild.channel.name(403801761787281409)
+	karaoke = Guild.channels.get(315219359368740875).toString() 
+	imitation = Guild.channels.get(403801761787281409).toString()
 
 	embed = discord.Embed(colour = discord.Colour(0xC21C1C))
 	embed.set_author(name = "PLANIFICATION DES EVENEMENTS") 
