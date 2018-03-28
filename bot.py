513314cfdd5714936ab687e471b869d845161254
Mitohku <34718938+Mitohku@users.noreply.github.com>
2018-03-28 16:06:04 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -68,8 +68,8 @@ async def staff(ctx):
 	embed7 = discord.Embed(colour = discord.Colour(0xC21C1C))
 	embed8 = discord.Embed(colour = discord.Colour(0xC21C1C))
 	embed9 = discord.Embed(colour = discord.Colour(0xC21C1C))
-	texte1 = "ㅤ\nㅤ\n***/!\ ROLES & DESCRIPTION DU STAFF /!\***\nㅤ"
-	texte2 = "ㅤ\nㅤ\n***/!\ ROLES & DESCRIPTION DU NOYAU DUR /!\***\nㅤ"
+	texte1 = "***/!\ ROLES & DESCRIPTION DU STAFF /!\***\nㅤ"
+	texte2 = "ㅤ\nㅤ\nㅤ\n***/!\ ROLES & DESCRIPTION DU NOYAU DUR /!\***\nㅤ"
 	
 
 	await ctx.send(texte1)
