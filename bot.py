6e869201e48ea4efffb5ec998f800bc0cda6defe
Mitohku <34718938+Mitohku@users.noreply.github.com>
2018-03-28 09:08:30 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -31,10 +31,10 @@ async def on_ready():
 @bot.command()
 async def quickpoll(ctx, question, *options: str):
         if len(options) <= 1:
-            await ctx.bot.say('You need more than one option to make a poll!')
+            await ctx.send('You need more than one option to make a poll!')
             return
         if len(options) > 10:
-            await ctx.bot.say('You cannot make a poll for more than 10 things!')
+            await ctx.send('You cannot make a poll for more than 10 things!')
             return
 
         if len(options) == 2 and options[0] == 'yes' and options[1] == 'no':
@@ -46,7 +46,7 @@ async def quickpoll(ctx, question, *options: str):
         for x, option in enumerate(options):
             description += '\n {} {}'.format(reactions[x], option)
         embed = discord.Embed(title=question, description=''.join(description))
-        react_message = await ctx.bot.say(embed=embed)
+        react_message = await ctx.send(embed=embed)
         for reaction in reactions[:len(options)]:
             await ctx.bot.add_reaction(react_message, reaction)
         embed.set_footer(text='Poll ID: {}'.format(react_message.id))
