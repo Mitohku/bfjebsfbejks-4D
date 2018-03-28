ab4f4c32404f876664f46c4b3f722e6dac2b895d
Mitohku <34718938+Mitohku@users.noreply.github.com>
2018-03-28 18:04:41 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -127,9 +127,9 @@ async def event(ctx):
 
 	embed = discord.Embed(colour = discord.Colour(0xC21C1C))
 	embed.set_author(name = "PLANIFICATION DES EVENEMENTS") 
-	embed.add_field(name="Gaming'Actu", value= f"Samedi 31 Mars, 17h-18h, **(FirstStation)**\n*Animé par @Say'  [ Hypesia.net ]#9257*", inline=False)
-	embed.add_field(name="Karaoké", value= f"Tous les Samedis, 21h, **({karaoke})**\n*Animé par @Shi ~#9262, @יRεγss ღ#4081, @Alone#5902*", inline=False)
-	embed.add_field(name="Imitations Vocales", value= f"Dimanche 25 Mars, 21h, **({imitation})**\n*Animé par @J.Ɛ.Ɲ.I.#7795, @Ashley#4026*", inline=False)
+	embed.add_field(name="Gaming'Actu", value= f"Samedi 31 Mars, 17h-18h, **(FirstStation)**\n*Animé par **Say' [ Hypesia.net ]***", inline=False)
+	embed.add_field(name="Karaoké", value= f"Tous les Samedis, 21h, **({karaoke})**\n*Animé par **K**, **Reyss**, **Alone***", inline=False)
+	embed.add_field(name="Imitations Vocales", value= f"Dimanche 25 Mars, 21h, **({imitation})**\n*Animé par **J.Ɛ.Ɲ.I.**, **Ashley***", inline=False)
 	embed.set_footer(text = "| ©  Say'")
 
 	await ctx.send(embed = embed)
